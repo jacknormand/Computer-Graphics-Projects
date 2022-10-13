dc6b05d981f2f6c1992a929d4daab0d734545164
jacknormand <jack.normand15@gmail.com>
2022-10-12 23:21:18 -0700

    beginning of raycaster

--- CS430Projects/cs430_3_jen88_shf26/ray.c
+++ CS430Projects/cs430_3_jen88_shf26/ray.c
@@ -1,6 +1,5 @@
 #include<stdio.h>
 #include "v3math.h"
-int main();
 
 // TODO 
 // parse input.csv
@@ -16,23 +15,45 @@ int main();
 typedef struct ray
 {
     // ray origin (A) (vector) (0,0,0 for camera)
-    float origin[3];
+    float* origin;
 
     // ray direction (B) (vector) (along z axis for camera)
-    float direction[3];
+    float* direction;
 
     // float num (t) (distance from origin?)
-    int distance;
+    //int distance;
 
     // p(t) = A + t*B (p is position) (points at stuff) (might be moved outta here idk)
     // all the points p lying on the ray satisfy the ray equation
 
 }ray;
 
+int main();
+void getColor(float* dst, ray inputRay);
+
+void getColor(float* dst, ray inputRay)
+{
+    // nomralize direction
+    float normalized[] = {0,0,0};
+    // set colors
+    float colorWhite[] = {1.0,1.0,1.0};
+    float colorRed[] = {.5,.7,1.0};
+    float finalColor[] = {0,0,0};
+    v3_normalize(normalized, inputRay.direction);
+
+    // get it done
+    float lenT = .5 * (normalized[1] + 1.0);
+    float neglenT = 1 - lenT;
+    v3_scale(colorWhite, neglenT);
+    v3_scale(colorRed, lenT);
+    v3_add(dst, colorRed, colorWhite);
+
+}
 
 
-int main() {
 
+int main()
+{
     // these values read from arg in later
     int sceneWidth = 400;
     int sceneHeight = 400;
@@ -62,7 +83,8 @@ int main() {
 
 
     // background color (need to divide by 255 for math, then multiply by 255 to get back to value to write)
-    // float pixelColor[] = {80,200,255};
+    // this changes btw these numbers are a default blue
+    float pixelColor[] = {80,200,255};
 
     char* outputFileName = "hello.ppm";
     FILE* outputFile;
@@ -78,10 +100,21 @@ int main() {
     // index vars
     float widthIndex;
     float heightIndex;
+    float xVal;
+    float yVal;
+    float directionVar[] = {lowerLeft[0],lowerLeft[1],lowerLeft[2]};
+    float horizontalTimesX[] = {horizontal[0],horizontal[1],horizontal[2]};
+    float verticalTimesY[] = {vertical[0],vertical[1],vertical[2]};
+
+    // ray
+    ray currentRay;
 
     // iterate over pixels in image, one a time,  shooting a ray 
     // through the center of the pixel out into the scene, 
     // looking for intersections between each ray and the scene geometry.
+    //printf("1:%f\n", pixelColor[0]);
+    //printf("2:%f\n", pixelColor[1]);
+    //printf("3:%f\n", pixelColor[2]);
 
     // For loop vertical
     for ( heightIndex = sceneHeight-1; heightIndex >= 0; heightIndex--)
@@ -89,13 +122,42 @@ int main() {
         // For loop horizontal
         for (widthIndex = 0; widthIndex < sceneWidth; widthIndex++)
         {
-            pixelColor[0] = (widthIndex/(sceneWidth-1));
-            //printf("1:%f\n", pixelColor[0]);
-            pixelColor[1] = (heightIndex/(sceneHeight-1));
-            //printf("2:%f\n", pixelColor[1]);
-            pixelColor[2] = .25;
-            //printf("3:%f\n", pixelColor[2]);
+            xVal = (widthIndex/(sceneWidth-1));
+            yVal = (heightIndex/(sceneHeight-1));
+
+            // set ray origin (should be 0 by default but whatever set anyways)
+            currentRay.origin = origin;
+
+            // do some quick maf
+            v3_scale(horizontalTimesX, xVal);
+            v3_scale(verticalTimesY, yVal);
+            v3_add(directionVar, horizontalTimesX, verticalTimesY);
+            v3_subtract(directionVar, directionVar, origin);
+
+            // set ray direciton
+            currentRay.direction = directionVar;
+
+            //currentRay.direction = directionVar;
+            // printf("1:%f\n", currentRay.direction[0]);
+            // printf("2:%f\n", currentRay.direction[1]);
+            // printf("3:%f\n", currentRay.direction[2]);
+
+            // get ray color 
+            getColor(pixelColor, currentRay);
+            
             fprintf(outputFile, "%f %f %f ", pixelColor[0]*255, pixelColor[1]*255, pixelColor[2]*255);
+
+            // reset (this is ugly ik)
+            directionVar[0] = lowerLeft[0];
+            directionVar[1] = lowerLeft[1];
+            directionVar[2] = lowerLeft[2];
+            horizontalTimesX[0] = horizontal[0];
+            horizontalTimesX[1] = horizontal[1];
+            horizontalTimesX[2] = horizontal[2];
+            verticalTimesY[0] = vertical[0];
+            verticalTimesY[1] = vertical[1];
+            verticalTimesY[2] = vertical[2];
+            
         }
         fprintf(outputFile, "\n");
     }
